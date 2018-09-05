251f9ed7944ef0c897d70b58b42be562ea5ae046
Ceoces <2218722428@qq.com>
2018-09-05 21:44:30 +0800

    添加修改教师功能

--- teachertable.php
+++ teachertable.php
@@ -94,7 +94,7 @@
                         }
                         echo "<td>".$row[$i]['phone']."</td>";
                         echo "<td>".$row[$i]['email']."</td>";
-                        echo "<td><button class='btn btn-warning btn-sm'>修改</button>&nbsp;&nbsp;&nbsp;&nbsp;<a href='delete.php?obj=teacher&id=".$row[$i]['id']."'><button class='btn btn-danger btn-sm'>删除</button></a></td>";
+                        echo "<td><a href='changeteacher.php?id=".$row[$i]['id']."'><button class='btn btn-warning btn-sm'>修改</button></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='delete.php?obj=teacher&id=".$row[$i]['id']."'><button class='btn btn-danger btn-sm'>删除</button></a></td>";
                         echo "</tr>";
                       }
                  ?>
