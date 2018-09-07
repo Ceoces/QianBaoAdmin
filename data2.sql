918f923b5a4e6e5bfee07ea8266bbce612bfc306
Ceoces <2218722428@qq.com>
2018-09-07 20:21:05 +0800

    数据库更新
    修改student表结构

--- data2.sql
+++ data2.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50547
  File Encoding         : 65001
 
- Date: 05/09/2018 14:42:27
+ Date: 07/09/2018 20:18:25
 */
 
 SET NAMES utf8mb4;
@@ -52,15 +52,16 @@ CREATE TABLE `t_project`  (
   `maxStuNum` int(11) NULL DEFAULT NULL COMMENT '最多加入学生个数',
   `startTime` datetime NOT NULL COMMENT '项目开始时间',
   `endTime` datetime NULL DEFAULT NULL COMMENT '项目截止时间',
+  `static` int(11) NULL DEFAULT 0 COMMENT '项目状态 0未开始 1正在进行 2已经结束 3异常结束',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = MyISAM AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_project
 -- ----------------------------
-INSERT INTO `t_project` VALUES (1, '实验室签报系统', 'admin', 1, '开放实验室签报系统', 30, '2018-07-24 09:18:56', '2018-09-29 09:18:59');
-INSERT INTO `t_project` VALUES (2, '项目2', 'su', 2, NULL, 10, '2018-07-24 10:41:13', '2018-07-28 10:41:16');
-INSERT INTO `t_project` VALUES (3, '测试项目a', 'admin', 2, '测试项目介绍', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `t_project` VALUES (1, '实验室签报系统', 'admin', 1, '开放实验室签报系统', 30, '2018-07-24 09:18:56', '2018-09-29 09:18:59', NULL);
+INSERT INTO `t_project` VALUES (2, '项目2', 'su', 2, NULL, 10, '2018-07-24 10:41:13', '2018-07-28 10:41:16', NULL);
+INSERT INTO `t_project` VALUES (3, '测试项目a', 'admin', 2, '测试项目介绍', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', NULL);
 
 -- ----------------------------
 -- Table structure for t_signtable
@@ -108,17 +109,21 @@ CREATE TABLE `t_stu_laboratory`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `studentid` char(11) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `laboratoryid` int(11) NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `time_in_lab` datetime NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '加入时间',
+  `time_in_lab` datetime NOT NULL COMMENT '累计时间',
+  `static` int(255) NOT NULL DEFAULT 1 COMMENT '0离开1在实验室2离校',
+  `info` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Fixed;
+) ENGINE = MyISAM AUTO_INCREMENT = 9 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_stu_laboratory
 -- ----------------------------
-INSERT INTO `t_stu_laboratory` VALUES (6, '3', 2, '2018-08-02 13:37:10', '0000-00-00 00:00:00');
-INSERT INTO `t_stu_laboratory` VALUES (5, '2', 1, '2018-08-02 13:02:59', '0000-00-00 00:00:00');
-INSERT INTO `t_stu_laboratory` VALUES (4, '1', 1, '2018-08-02 13:02:53', '0000-00-00 00:00:00');
+INSERT INTO `t_stu_laboratory` VALUES (6, '3', 2, '2018-08-02 13:37:10', '0000-00-00 00:00:00', 1, NULL);
+INSERT INTO `t_stu_laboratory` VALUES (5, '2', 1, '2018-08-02 13:02:59', '0000-00-00 00:00:00', 1, NULL);
+INSERT INTO `t_stu_laboratory` VALUES (4, '1', 1, '2018-08-02 13:02:53', '0000-00-00 00:00:00', 1, NULL);
+INSERT INTO `t_stu_laboratory` VALUES (7, '123456', 1, '2018-09-05 19:32:42', '0000-00-00 00:00:00', 1, NULL);
+INSERT INTO `t_stu_laboratory` VALUES (8, '3', 1, '2018-09-07 19:38:03', '0000-00-00 00:00:00', 1, NULL);
 
 -- ----------------------------
 -- Table structure for t_stu_pro
@@ -130,14 +135,16 @@ CREATE TABLE `t_stu_pro`  (
   `proid` int(11) NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 8 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Fixed;
+) ENGINE = MyISAM AUTO_INCREMENT = 10 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Fixed;
 
 -- ----------------------------
 -- Records of t_stu_pro
 -- ----------------------------
+INSERT INTO `t_stu_pro` VALUES (8, '123456', 1, '2018-09-05 19:24:22');
 INSERT INTO `t_stu_pro` VALUES (7, '123', 1, '2018-09-04 19:36:54');
 INSERT INTO `t_stu_pro` VALUES (6, '2', 1, '2018-08-02 13:03:14');
 INSERT INTO `t_stu_pro` VALUES (5, '1', 1, '2018-08-02 13:02:19');
+INSERT INTO `t_stu_pro` VALUES (9, '17140303xx', 1, '2018-09-05 19:30:54');
 
 -- ----------------------------
 -- Table structure for t_student
@@ -146,13 +153,10 @@ DROP TABLE IF EXISTS `t_student`;
 CREATE TABLE `t_student`  (
   `id` char(11) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '学号\r\n',
   `teacherid` char(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '教师id',
-  `laboratoryid` int(11) NOT NULL COMMENT '实验室id',
-  `proid` int(11) NULL DEFAULT NULL COMMENT '项目id',
   `name` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '姓名',
   `class` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '班级',
   `password` char(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码 （sha1加密）',
-  `addtime` datetime NOT NULL COMMENT '加入时间',
-  `leavetime` datetime NULL DEFAULT NULL COMMENT '离开时间',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '加入时间',
   `sex` int(1) NULL DEFAULT NULL COMMENT '性别 （1：男  0：女）',
   `phone` char(12) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '电话',
   `email` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮箱',
@@ -165,10 +169,12 @@ CREATE TABLE `t_student`  (
 -- ----------------------------
 -- Records of t_student
 -- ----------------------------
-INSERT INTO `t_student` VALUES ('3', 'admin', 2, 2, '赵恩伯', '集成17-1', '77de68daecd823babbb58edb1c8e14d7106e83bb', '0000-00-00 00:00:00', NULL, 1, '15561858955', '2218722428@qq.com', 0, NULL, 1);
-INSERT INTO `t_student` VALUES ('1', 'su', 1, 1, '杨恒', '1111', '2ea6201a068c5fa0eea5', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, NULL, NULL, 0, NULL, 1);
-INSERT INTO `t_student` VALUES ('2', 'admin', 1, 1, '梁成辉', '123456', '7c4a8d09ca3762af61e5', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, NULL, NULL, 0, NULL, 1);
-INSERT INTO `t_student` VALUES ('123', 'su', 1, NULL, '学生A', '集成17-1', '40bd001563085fc35165', '2018-09-04 00:00:00', '2018-09-30 00:00:00', NULL, NULL, NULL, 0, NULL, 1);
+INSERT INTO `t_student` VALUES ('3', 'admin', '赵恩伯', '集成17-1', '77de68daecd823babbb58edb1c8e14d7106e83bb', '0000-00-00 00:00:00', 1, '15561858955', '2218722428@qq.com', 0, NULL, 1);
+INSERT INTO `t_student` VALUES ('1', 'su', '杨恒', '1111', '2ea6201a068c5fa0eea5', '0000-00-00 00:00:00', 1, NULL, NULL, 0, NULL, 1);
+INSERT INTO `t_student` VALUES ('2', 'admin', '梁成辉', '123456', '7c4a8d09ca3762af61e5', '0000-00-00 00:00:00', 1, NULL, NULL, 0, NULL, 1);
+INSERT INTO `t_student` VALUES ('123', 'su', '学生A', '集成17-1', '40bd001563085fc35165', '2018-09-04 00:00:00', NULL, NULL, NULL, 0, NULL, 1);
+INSERT INTO `t_student` VALUES ('123456', 'admin', '123', '1111', '7c4a8d09ca3762af61e59520943dc26494f8941b', '0000-00-00 00:00:00', NULL, NULL, NULL, 0, NULL, 1);
+INSERT INTO `t_student` VALUES ('17140303xx', 'admin', '汪筠博', '电科17-3', '00991a28e7c1a3cff9781b7769d54174b05a1817', '0000-00-00 00:00:00', NULL, NULL, NULL, 0, NULL, 1);
 
 -- ----------------------------
 -- Table structure for t_teacher
@@ -258,25 +264,23 @@ INNER JOIN v_student ON v_student.id = t_signtable.stuid ;
 -- ----------------------------
 DROP VIEW IF EXISTS `v_student`;
 CREATE ALGORITHM = UNDEFINED DEFINER = `root`@`localhost` SQL SECURITY DEFINER VIEW `v_student` AS SELECT
-t_student.id,
 t_student.`name` AS stuname,
-t_teacher.`name` AS teachername,
 t_student.class,
-t_student.`password`,
-t_student.addtime,
-t_student.leavetime,
 t_student.sex,
 t_student.phone,
 t_student.email,
-t_student.info,
+t_teacher.`name` AS teachername,
+t_project.`name` AS proname,
 t_laboratory.`name` AS laboratoryname,
-t_teacher.id AS teacherid,
-t_project.`name` AS proname
+t_student.id,
+t_student.addtime
 FROM
 t_student
-INNER JOIN t_teacher ON t_teacher.id = t_student.teacherid
-INNER JOIN t_laboratory ON t_student.laboratoryid = t_laboratory.id
-INNER JOIN t_project ON t_student.proid = t_project.id ;
+INNER JOIN t_teacher ON t_student.teacherid = t_teacher.id
+INNER JOIN t_stu_pro ON t_student.id = t_stu_pro.studentid
+INNER JOIN t_project ON t_stu_pro.proid = t_project.id
+INNER JOIN t_stu_laboratory ON t_student.id = t_stu_laboratory.studentid
+INNER JOIN t_laboratory ON t_stu_laboratory.laboratoryid = t_laboratory.id ;
 
 -- ----------------------------
 -- View structure for v_stu_laboratory
@@ -287,7 +291,6 @@ t_laboratory.`name` AS laboratoryname,
 t_student.`name` AS stuname,
 t_stu_laboratory.time,
 t_student.class,
-t_project.`name` AS proname,
 t_student.sex,
 t_student.phone,
 t_student.email,
@@ -297,8 +300,7 @@ t_stu_laboratory.laboratoryid
 FROM
 t_laboratory
 INNER JOIN t_stu_laboratory ON t_stu_laboratory.laboratoryid = t_laboratory.id
-INNER JOIN t_student ON t_student.id = t_stu_laboratory.studentid
-INNER JOIN t_project ON t_project.id = t_student.proid ;
+INNER JOIN t_student ON t_student.id = t_stu_laboratory.studentid ;
 
 -- ----------------------------
 -- View structure for v_stu_pro
