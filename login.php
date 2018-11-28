8edbdd8ea7527c37dc1e4bda7bd7775206013164
Ceoces <2218722428@qq.com>
2018-11-28 16:27:23 +0800

    修改无法跳转的bug

--- login.php
+++ login.php
@@ -1,26 +1,4 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <link rel="shortcut icon" href="images/favicon.png" type="image/png">
-
-  <title>实验室签报系统后台</title>
-
-  <link href="css/style.default.css" rel="stylesheet">
-
-  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!--[if lt IE 9]>
-  <script src="js/html5shiv.js"></script>
-  <script src="js/respond.min.js"></script>
-  <![endif]-->
-</head>
-
-<body class="signin">
-  <?php
+<?php
   require_once('360webscan.php');
   session_start();
   $_SESSION['id']='';
@@ -51,13 +29,35 @@
     }
     else
     {
-      header('location:login.php?error=1');
       $_SESSION['id']="";
       $_SESSION['pwd']="";
+      header('location:login.php?error=1');
      }
     }
   }   
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="shortcut icon" href="images/favicon.png" type="image/png">
+
+  <title>实验室签报系统后台</title>
+
+  <link href="css/style.default.css" rel="stylesheet">
+
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+  <script src="js/html5shiv.js"></script>
+  <script src="js/respond.min.js"></script>
+  <![endif]-->
+</head>
+
+<body class="signin">
+  
 
 <!-- Preloader -->
 <div id="preloader">
